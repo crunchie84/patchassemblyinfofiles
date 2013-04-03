5d1146154716bcc0bf50acdfb13a17273e0bd0b9
Mark van Straten <mark@q42.nl>
2013-04-03 10:49:23 +0200

    initial version

---
+++ PatchAssemblyInfoFiles/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using PowerArgs;
+
+namespace PatchAssemblyInfoFiles
+{
+  class Arguments
+  {
+    [ArgRequired(PromptIfMissing = false)]
+    [ArgExistingDirectory]
+    public string sourceBasePath { get; set; }
+
+    [ArgDescription("version file which contains the version 1.2.3.4 string")]
+    [ArgRequired(PromptIfMissing = false)]
+    [ArgExistingFile]
+    public string versionFile { get; set; }
+    
+    //[ArgDescription("version 1.2.3.4 can be supplied directly instead of version file")]
+    //public string version { get; set; }
+
+    [ArgDescription("file containing the user-friendly version information string (one liner)")]
+    [ArgRequired(PromptIfMissing = false)]
+    [ArgExistingFile]
+    public string informalVersionFile { get; set; }
+
+    //[ArgDescription("user-friendly version information string")]
+    //public string informalVersion { get; set; }
+
+    public string companyName { get; set; }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("PatchAssemblyInfoFiles.exe");
+      Console.WriteLine("This program searches in the specified dir + subdirs for AssemblyInfo.cs files");
+      Console.WriteLine("And modifies the existing attributes with the specified values, or if the attributes are missing appends these");
+      Console.WriteLine("");
+      try
+      {
+        var parsed = Args.Parse<Arguments>(args);
+
+        string versionString = System.IO.File.ReadAllText(parsed.versionFile).Trim().Replace(Environment.NewLine, "");
+        Version version = new Version(versionString);
+
+        string informalVersionString = System.IO.File.ReadAllText(parsed.informalVersionFile).Trim().Replace(Environment.NewLine, "");
+
+        Console.Write(Environment.NewLine + Environment.NewLine);
+        Console.WriteLine("Passed arguments (evaluated): ");
+        Console.WriteLine(" - sourceBasePath=" + parsed.sourceBasePath);
+        Console.WriteLine(" - version=" + version);
+        Console.WriteLine(" - company=" + parsed.companyName);
+        Console.WriteLine(" - informalversion=" + informalVersionString);
+        Console.Write(Environment.NewLine);
+
+        //find all assemblyinfo files
+        var assemblyInfoFiles = Directory.GetFiles(parsed.sourceBasePath, "AssemblyInfo.cs", SearchOption.AllDirectories);
+        Console.WriteLine(string.Format("Found {0} AssemblyInfo.cs files in source dir, going to patch these{1}", assemblyInfoFiles.Length, Environment.NewLine));
+
+        foreach (string assemblyInfoFile in assemblyInfoFiles)
+        {
+          //open file, find attributes, replace content or add attributes
+          FileInfo file = new FileInfo(assemblyInfoFile);
+          file.IsReadOnly = false;//justin case
+
+          string filecontents = System.IO.File.ReadAllText(assemblyInfoFile);
+
+          if (!string.IsNullOrWhiteSpace(parsed.companyName))
+          {
+            string companyName = parsed.companyName.Replace('"', '\'');
+            updateOrAddProperty(ref filecontents, "AssemblyCompany", companyName);
+            updateOrAddProperty(ref filecontents, "AssemblyCopyright", "Copyright ©" + companyName + " " + DateTime.Now.Year);
+          }
+          
+          updateOrAddProperty(ref filecontents, "AssemblyVersion", version.ToString());
+          updateOrAddProperty(ref filecontents, "AssemblyFileVersion", version.ToString());
+          updateOrAddProperty(ref filecontents, "AssemblyInformationalVersion", informalVersionString);
+
+          System.IO.File.WriteAllText(assemblyInfoFile, filecontents);
+          Console.WriteLine(" - patched " + assemblyInfoFile);
+        }
+      }
+      catch (ArgException ex)
+      {
+        Console.WriteLine(ex.Message);
+        Console.WriteLine(ArgUsage.GetUsage<Arguments>());
+        Environment.Exit(1);
+      }
+      catch (Exception ex)
+      { 
+        Console.WriteLine(ex.ToString());
+        Environment.Exit(1);
+      }
+    }
+
+    private static void updateOrAddProperty(ref string filecontents, string propertyname, string value)
+    {
+      string pattern = "[assembly: "+propertyname+"(\"";
+      if (filecontents.Contains(pattern))
+      {
+        updateFileContents(ref filecontents, 0, pattern, value);
+      }
+      else
+      {
+        //append new attribute
+        filecontents += string.Format(@"{2}//added by PatchAssemblyInfoFiles.exe{2}[assembly: {0}(""{1}"")]", propertyname, value.Trim(), Environment.NewLine);
+      }
+    }
+
+    private static void updateFileContents(ref string filecontents, int startIndex, string pattern, string value)
+    {
+      int startAt = filecontents.IndexOf(pattern, startIndex);
+      if (startAt == -1)
+        return;
+        
+      startAt += pattern.Length;
+      int EndAd = filecontents.IndexOf("\")]", startAt);
+      filecontents = filecontents.Remove(startAt, EndAd - startAt);//remove old crap
+      filecontents = filecontents.Insert(startAt, value.Trim());//insert new stuff at start index
+
+      updateFileContents(ref filecontents, EndAd, pattern, value);//maybe multiple values available?
+    }
+  }
+}
---
+++ PatchAssemblyInfoFiles/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PatchAssemblyInfoFiles")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("q42")]
+[assembly: AssemblyProduct("PatchAssemblyInfoFiles")]
+[assembly: AssemblyCopyright("Copyright ©q42 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b2055599-2a92-4cbf-a6a8-01cd8dfc6abd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.2.3.4")]
+[assembly: AssemblyVersion("1.2.3.4")]
+[assembly: AssemblyFileVersion("1.2.3.4")]
+
+//added by PatchAssemblyInfoFiles.exe
+[assembly: AssemblyInformationalVersion("foo bar baz 1 2 3 4")]
